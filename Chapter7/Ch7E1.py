5abe8d85f0205e37e1d9275b93d3c8c4d63c7021
ImanTabari <tabari.iman@gmail.com>
2022-03-08 12:09:10 +0100

    Chapter 7 exersices are added.

--- Chapter7/Ch7E1.py
+++ Chapter7/Ch7E1.py
@@ -1,1 +1,23 @@
-print ('hello')
+'''
+Write a program to read through a file and print the contents of the
+file (line by line) all in upper case. Executing the program will look as follows:
+python shout.py
+Enter a file name: mbox-short.txt
+FROM STEPHEN.MARQUARD@UCT.AC.ZA SAT JAN 5 09:14:16 2008
+RETURN-PATH: <POSTMASTER@COLLAB.SAKAIPROJECT.ORG>
+7.11. Exercises 89
+RECEIVED: FROM MURDER (MAIL.UMICH.EDU [141.211.14.90])
+BY FRANKENSTEIN.MAIL.UMICH.EDU (CYRUS V2.3.8) WITH LMTPA;
+SAT, 05 JAN 2008 09:14:16 -0500
+'''
+#import os
+#print(os.getcwd())
+fname = input('Please enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('your file name %s dose not exist, please try agin.' %fname)
+    exit()
+for line in fhand:
+   print(line.rstrip().upper())
+
---
+++ Chapter7/Ch7E2.py
@@ -0,0 +1,26 @@
+'''
+Write a program to prompt for a file name, and then read through
+the file and look for lines of the form:
+X-DSPAM-Confidence: 0.8475
+When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart
+the line to extract the floating-point number on the line. Count these lines and
+then compute the total of the spam confidence values from these lines. When you
+reach the end of the file, print out the average spam confidence.
+'''
+fname = input('Please enter your file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('Your file name %s does not exist. please try again.' %fname)
+    exit()
+total_spam_line = 0
+total_spam_value = 0
+for line in fhand:
+    if line.lower().startswith('x-dspam-confidence'):
+    #if not line.lower().startswith('x-dspam-confidence'): continue    
+        total_spam_line += 1
+        total_spam_value += float(line[line.lstrip().find(' ')+1:])
+#print('Avarage spam confidence is:' , total_spam_value/total_spam_line)
+print('The avarage spam confidence in %s file is %g' %(fname, total_spam_value/total_spam_line))
+
+
